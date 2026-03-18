57e8acbd1a13d86e238aad771a2f46009c32bda6
joshuaroy873 <joshuaroy873@gmail.com>
2026-03-17 21:42:13 -0400

    minor update

--- include/ethernet_frame.hpp
+++ include/ethernet_frame.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include <cstdint>
 
+#define MAC_ADDRESS_SIZE 6
+
 struct EthernetFrame {
-    uint8_t dst_mac[6];
-    uint8_t src_mac[6];
+    uint8_t dst_mac[MAC_ADDRESS_SIZE];
+    uint8_t src_mac[MAC_ADDRESS_SIZE];
     uint16_t ether_type;
     int payload_size;
     uint8_t* p_payload;
